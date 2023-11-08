6a67f7f4bbdab8b0fd6ab2d9eefe0f7a123d01ef
Omar Bautista <bautistaramirezomar0@gmail.com>
2023-11-08 07:50:55 -0600

    EJERCICIO_1 terminado, revisar la salida

--- EJERCICIO_1.java
+++ EJERCICIO_1.java
@@ -9,28 +9,31 @@ Elaborado: 4/ 11/23*/
 public class EJERCICIO_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nums[] = new int[20];
-        int numerosPaIn[]=new int[20];
-        int i,aux=0;
+        int []nums= new int[20];
+        int []numerosPa=new int[20];
+        int []numerosIn=new int[20];
+
+        int i;
 
         for (i=0; i<nums.length; i++){
             nums[i]=0;
-            numerosPaIn[i]=0;
+            numerosPa[i]=0;
+            numerosIn[i]=0;
         }
 
         for (i=0; i< nums.length; i++){
             System.out.println("Ingresa los valores para ["+(i+1)+"] :");
             nums[i]= sc.nextInt();
-             aux = nums[i]*2;
-
             }
-        if (nums[i]==aux/2){
-            for (i=0; i< nums.length; i++){
-                numerosPaIn[i]=nums[i];
+    
+        for (i=0; i< nums.length; i++){
+            if (nums[i]%2==0){
+                    numerosPa[i]=nums[i];
+            }else{
+                numerosIn[i]=nums[i];
             }
+            System.out.println(numerosPa[i]+numerosIn[i]);
         }
-
-
 
 
     }
